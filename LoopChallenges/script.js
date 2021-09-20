e67a26562f1941e2806abaab107fe0a9b7bb1438
Austen Jensen <Austen.jensen@ymail.com>
2021-09-19 20:49:01 -0700

    initial commit

---
+++ LoopChallenges/script.js
@@ -0,0 +1,28 @@
+for (var x=1; x<=20; x++){
+    if (x%2!=0)
+    console.log (x)
+}
+
+for (var x =100; x>=0; x--){
+    if (x%3==0)
+    console.log (x)
+}
+
+for (var x=4; x>=-3.5; x-=1.5){
+    console.log(x)
+}
+
+var sum = 0
+    for (var x=1; x<=100; x++){
+    sum += +x;}
+{
+    console.log (sum)
+}
+
+var product = 1
+for (var x = 1; x<=12; x++){
+    product *= x;
+}
+{
+    console.log(product)
+}
