9af36d33a73deadd453daea9c25e555b43490ba2
Austen Jensen <Austen.jensen@ymail.com>
2021-09-20 15:45:42 -0700

    initial commit

---
+++ ButtonClicker/script.js
@@ -0,0 +1,9 @@
+function login(element){
+    element.innerText="Logout";
+}
+function hide(element){
+    element.remove();
+}
+function notice(element){
+    alert(innerText="ninja was liked");
+}
