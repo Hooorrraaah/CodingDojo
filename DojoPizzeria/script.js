fe9549262c4f68ac81a1f1addfdb84c40e5c427d
Austen Jensen <Austen.jensen@ymail.com>
2021-09-20 19:02:31 -0700

    initial commit

---
+++ DojoPizzeria/script.js
@@ -0,0 +1,25 @@
+
+var pizzaOven = {
+    "crustType": ["hand tossed","deep dish", "original", "thin"],
+    "sauceType": ["traditional", "white", "marinara","spicy","pesto"],
+    "cheeses": ["mozzarella", "feta", "cheddar","parmesan"],
+    "toppings": ["pepperoni", "sausage", "mushrooms", "olives", "onions"],
+}
+
+// function(){
+//     console.log (pizzaOven);
+// }
+// }
+
+    console.log ("Pizza: " + pizzaOven.crustType[1]+ ", " +pizzaOven.sauceType[0]+
+    ", "+pizzaOven.cheeses[0]+", "+pizzaOven.toppings[0]+", and "+pizzaOven.toppings[1]);
+
+    console.log ("Pizza: " + pizzaOven.crustType[0]+ ", " +pizzaOven.sauceType[2]+
+    ", "+pizzaOven.cheeses[0]+", "+pizzaOven.cheeses[1]+", "+pizzaOven.toppings[2]+", "+pizzaOven.toppings[3]+", and "+pizzaOven.toppings[4]);
+
+    console.log ("Pizza: " + pizzaOven.crustType[2]+ ", " +pizzaOven.sauceType[2]+
+    ", "+pizzaOven.cheeses[0]+", "+pizzaOven.toppings[2]+", "+pizzaOven.toppings[0]+", and "+pizzaOven.toppings[1]);
+
+    console.log ("Pizza: " + pizzaOven.crustType[3]+ ", " +pizzaOven.sauceType[3]+
+    ", "+pizzaOven.cheeses[2]+", "+pizzaOven.toppings[2]+", and "+pizzaOven.toppings[4]);
+
