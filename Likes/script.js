ec019e8406c8b5249a7ae49dd1a9c54788c94e9b
Austen Jensen <Austen.jensen@ymail.com>
2021-09-20 23:56:18 -0700

    initial commit

---
+++ Likes/script.js
@@ -0,0 +1,12 @@
+function like1 (i) {
+    document.querySelector ("#first");
+    first.innerText++;
+}    
+function like2 (i) {
+    document.querySelector ("#second");
+    second.innerText++;
+}
+function like3 (i) {
+    document.querySelector ("#third");
+    third.innerText++;
+}
