6bfad1f796b884c230556eb9d5032d09826d0580
Austen Jensen <Austen.jensen@ymail.com>
2021-09-21 14:17:34 -0700

    initial commit

---
+++ MakingConnections/script.js
@@ -0,0 +1,10 @@
+function hide(){
+    document.querySelector(".card-list-item").remove();
+}
+function hide1(){
+    document.querySelector(".card-list-item1").remove();
+}
+
+function change() {
+    document.querySelector(".card-body h1").innerText = "Input";
+}
